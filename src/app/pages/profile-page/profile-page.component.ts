e98e3b5cee2261180221cbd1e8e44e425a279717
Val Do <valeri.kharitonashvili1@gmail.com>
2024-06-01 14:20:02 +0400

    profile

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Title } from '@angular/platform-browser';
 import { AuthenticationService } from '../../services/authentication.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
@@ -14,7 +15,8 @@ import { AuthenticationService } from '../../services/authentication.service';
   imports: [
     NgIf,
     AsyncPipe,
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
@@ -26,13 +28,30 @@ export class ProfilePageComponent implements OnInit {
   private _title = inject(Title);
 
   public authenticationService = inject(AuthenticationService);
-  
-
   user$!: Observable<User>;
+
+  public personalInfoFormGroup: FormGroup = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern(new RegExp('^[a-zA-Z0-9]+$'))]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    about: new FormControl(''),
+  });
+
+  public emailAndPasswordFormGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    oldPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    newPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  error!: any;
 
   ngOnInit(): void {
     this.user$ = this._profileService.profile(this._route.snapshot.params['userName']).pipe(
-      tap(({ userName }) => this._title.setTitle(`mipov.net - ${userName}`)),
+      tap((user) => {
+        this.personalInfoFormGroup.patchValue(user);
+        this.emailAndPasswordFormGroup.patchValue(user);
+        this._title.setTitle(`mipov.net - ${user.userName}`)
+      }),
       catchError((errorResponse: HttpErrorResponse) => {
         if (errorResponse.status === 404) {
           this._router.navigate(['/error'], { queryParams: { error: errorResponse.error.message } });
@@ -40,5 +59,9 @@ export class ProfilePageComponent implements OnInit {
         throw errorResponse
       } )
     );
+  }
+
+  get userName(): FormControl {
+    return this.personalInfoFormGroup.controls['userName'] as FormControl;
   }
 }
