0fb0bfbf533739798d019dc66d4ba2487c2f62c9
Val Do <valeri.kharitonashvili1@gmail.com>
2024-06-06 22:23:25 +0400

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://206.189.56.215:5002'
-    // apiUrl: 'https://mipov.net/api'
+    // apiUrl: 'http://206.189.56.215:5002'
+    apiUrl: 'https://mipov.net/api'
 };
