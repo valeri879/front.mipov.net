7084a8ea560bfff450680365aff13adca99f8262
Val Do <valeri.kharitonashvili1@gmail.com>
2024-06-10 20:51:27 +0400

    buikld

--- dist/front.mipov.net/server/main.server.mjs
+++ dist/front.mipov.net/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7CUKFBCH.mjs";import"./chunk-Z2BVHSMA.mjs";import"./chunk-U67BR66E.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-DY5PXIDS.mjs";import"./chunk-Z2BVHSMA.mjs";import"./chunk-U67BR66E.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
